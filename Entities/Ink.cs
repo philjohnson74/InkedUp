9522aa4ecac1eb87f96f6fda2ae65c02a44cd94e
philjohnson74 <philliptjohnson@hotmail.com>
2021-03-15 19:45:05 +0000

    Added Pen and Ink Entities

---
+++ Entities/Ink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Ink
+    {
+        public Ink()
+        {
+        }
+
+        public string Manufacturer { get; set; }
+        public string Colour { get; set; }
+    }
+}
---
+++ Entities/Pen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Pen
+    {
+        public Pen()
+        {
+        }
+
+        public string Manufacturer { get; set; }
+        public Ink Ink { get; set; }
+    }
+}
